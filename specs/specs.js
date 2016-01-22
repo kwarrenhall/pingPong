01b3230b764f64a37643b3e5bf8d83e871395211
Kate Hall <kwarrenhall@gmail.com>
2016-01-22 12:01:01 -0800

    add failed test for divisible by three

--- specs/specs.js
+++ specs/specs.js
@@ -1,9 +1,12 @@
 
 describe("countUp", function(){
-it("will count from one to five", function() {
-    expect(countUp(5)).to.eql([1,2,3,4,5]);
+  it("will count from one to user specified number", function() {
+  expect(countUp(5)).to.eql([1,2,3,4,5]);
   });
-it("will count from one to a user specified number", function() {
-    expect(countUp(number)).to.eql(counter)
 });
+
+describe("divisibleBy3", function(){
+  it("will identify if a number is divisible by 3", function() {
+  expect(divisibleBy3(3)).to.eql(true);
+  });
 });
