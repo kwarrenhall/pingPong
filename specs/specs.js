bc5423b081df49c2115448b351dfebcb19b664af
Kate Hall <kwarrenhall@gmail.com>
2016-01-22 11:30:54 -0800

    add second countUp failed spec test will count up from one to a user specified number

--- specs/specs.js
+++ specs/specs.js
@@ -3,4 +3,7 @@ describe("countUp", function(){
 it("will count from one to five", function() {
     expect(countUp(5)).to.eql([1,2,3,4,5]);
   });
+it("will count from one to a user specified number", function() {
+    expect(countUp(number)).to.eql(counter)
+});
 });
