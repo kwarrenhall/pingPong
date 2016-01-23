8089d4989bd39688d5cdf8366021531cc85af211
Kate Hall <kwarrenhall@gmail.com>
2016-01-22 17:57:43 -0800

    wip front and back end

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
-var pingPong = function(number) {
+(document).ready(function(){
+  $("form").submit(function(event) {
+  var userNumber = parseInt($("input#number").val());
+  var result = pingPong(userNumber);
+  $("#outPut").text(result);
+  $(".result").show();
+  event.preventDefault();
+  });
+});
 
+
+var pingPong = function(number) {
+var outPut = [];
+for (number; number > 0; number++) {
     if (number % 3 ===0) {
-      return (true);}
-    if (number % 5 ===0) {
-      return (true);}
-    if (number % 3 === 0 && number % 5 ===0) {
-      return (true);}
-    else {
-      return (false);
+      outPut.push("ping");
+    } else if (number % 5 ===0) {
+      outPut.push("pong");}
+    } else if (number % 3 === 0 && number % 5 ===0) {
+      outPut.push("ping-pong");
+    } else {
+     outPut.push(number);
     }
 };
--- specs/specs.js
+++ specs/specs.js
@@ -1,11 +1,14 @@
 describe('pingPong', function() {
+  it("counts up to a specified user number", function () {
+    expect(pingPong(2)).to.eql([1,2])
+  });
   it("identifies numbers divisible by 3", function () {
-    expect(pingPong(3)).to.eql(true)
+    expect(pingPong(3)).to.eql([1,2,"ping"])
   });
   it("identifies numbers divisible by 5", function () {
-    expect(pingPong(5)).to.eql(true)
+    expect(pingPong(5)).to.eql([1,2,"ping",4,"Pong"])
   });
   it("identifies numbers divisible by 3 & 5", function () {
-    expect(pingPong(15)).to.eql(true)
+    expect(pingPong(15)).to.eql([1,2,"ping",4,"pong",6,7,8,9,10,11,12,13,14,"ping-pong"])
   });
 });
