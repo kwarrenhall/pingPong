fa091c3a31bed5e348c2038673957ba34cbaa46b
Kate Hall <kwarrenhall@gmail.com>
2016-01-22 16:43:48 -0800

    valid test of divisible by 3 & 5

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,10 @@ var pingPong = function(number) {
     if (number % 3 ===0) {
       return (true);}
     if (number % 5 ===0) {
-      return (true);
-    } else {
+      return (true);}
+    if (number % 3 === 0 && number % 5 ===0) {
+      return (true);}
+    else {
       return (false);
     }
-
-    };
+};
--- specs/specs.js
+++ specs/specs.js
@@ -4,5 +4,8 @@ describe('pingPong', function() {
   });
   it("identifies numbers divisible by 5", function () {
     expect(pingPong(5)).to.eql(true)
-});
+  });
+  it("identifies numbers divisible by 3 & 5", function () {
+    expect(pingPong(15)).to.eql(true)
+  });
 });
