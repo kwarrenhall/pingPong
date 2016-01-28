fbdc37a41e797ac833053e54c9e157990e90d6af
Kate Hall <kwarrenhall@gmail.com>
2016-01-28 13:42:02 -0800

    add simple version of code

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,46 @@
+//
+//
+// for (var i = 0; i >= 30; i++) {
+// if (i % 15 === 0) {
+//   return ("ping-pong")
+//
+//   //  if (i % 15 === 0) {
+//   //  return ("pingpong"); }
+//    }
+// };
 
-var pingPong = function(number) {
-var outPut = [];
-for (var i = 1; 1 >= number; i++) {
-    if (i % 3 ===0) {
-      outPut.push("ping");
-    } else if (i % 5 ===0) {
-      outPut.push("pong");
-    } else if (i % 3 === 0 && i % 5 ===0) {
-      outPut.push("ping-pong");
-    } else {
-     outPut.push(i);
-    }
-  }
-  return outPut;
-};
+  //  } else if (i % 5 ===0) {
+  //    return ("pong");
+  //  } else if (i % 3 ===0) {
+  //    return ("ping");
+  //  } else {
+  //    return (i);
+  //   }
+//   }
+// };
+//
+// (document).ready(function(){
+//   $("#userInput").submit(function(event) {
+//   var number = parseInt($("#number").val());
+//   var result = pingPong(number);
+//
+//   $("#outPut").show();
+//   event.preventDefault();
+//   });
+// });
 
-(document).ready(function(){
-  $("#userInput").submit(function(event) {
-  var number = parseInt($("#number").val());
-  var result = pingPong(number);
+var num = $('#userImput').val()
 
-  $("#outPut").show();
-  event.preventDefault();
-  });
-});
+   for (var i = 1; i <= num; i++) {
+     if (i % 15 === 0) {
+       $('#list').append('<li>'+"pingpong"+'</li>');}
+     else if (i % 3 === 0) {
+       $('#list').append('<li>'+"ping"+'</li>');}
+     else if (i % 5 === 0) {
+       $('#list').append('<li>'+"pong"+'</li>');}
+     else{
+       $('#list').append('<li>'+i+'</li>');}
+
+   };
+
+ // event.preventDefault();
--- specs/specs.js
+++ specs/specs.js
@@ -1,14 +1,14 @@
 describe('pingPong', function() {
-  it("counts up to a specified user number", function () {
-    expect(pingPong(2)).to.eql([1,2])
+   it("identifies numbers divisible by 15", function() {
+   expect(pingPong(15)).to.equal("pingpong");
+   });
+  it("identifies numbers divisible by 5", function() {
+    expect(pingPong(5)).to.eql("pong")
   });
-  it("identifies numbers divisible by 3", function () {
-    expect(pingPong(3)).to.eql([1,2,"ping"])
+  it("identifies numbers divisible by 3", function() {
+    expect(pingPong(3)).to.eql("ping")
   });
-  it("identifies numbers divisible by 5", function () {
-    expect(pingPong(5)).to.eql([1,2,"ping",4,"Pong"])
-  });
-  it("identifies numbers divisible by 3 & 5", function () {
-    expect(pingPong(15)).to.eql([1,2,"ping",4,"pong",6,7,8,9,10,11,12,13,14,"ping-pong"])
+  it("counts up to a specified user number", function() {
+    expect(pingPong(2)).to.eql(2)
   });
 });
