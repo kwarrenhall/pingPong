f2fd990aafef9bac6e8900b2fc9d576c3022ffd8
Kate Hall <kwarrenhall@gmail.com>
2016-01-28 20:22:23 -0800

    add first passing spec test for divisible by 15

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,12 @@
+
+// var pingPong = function (myNumber) {
 //
-//
-// for (var i = 0; i >= 30; i++) {
-// if (i % 15 === 0) {
-//   return ("ping-pong")
-//
-//   //  if (i % 15 === 0) {
-//   //  return ("pingpong"); }
+//    for (var i = 1; i <= myNumber; i++) {
+//      if (i % 15 === 0) {
+//      myNumber = ""
+//      myNumber += "pingpong"
+//      return myNumber;
 //    }
-// };
-
-  //  } else if (i % 5 ===0) {
-  //    return ("pong");
-  //  } else if (i % 3 ===0) {
-  //    return ("ping");
-  //  } else {
-  //    return (i);
-  //   }
-//   }
-// };
-//
-// (document).ready(function(){
-//   $("#userInput").submit(function(event) {
-//   var number = parseInt($("#number").val());
-//   var result = pingPong(number);
-//
-//   $("#outPut").show();
-//   event.preventDefault();
-//   });
-// });
-
-var num = $('#userImput').val()
-
-   for (var i = 1; i <= num; i++) {
-     if (i % 15 === 0) {
-       $('#list').append('<li>'+"pingpong"+'</li>');}
-     else if (i % 3 === 0) {
-       $('#list').append('<li>'+"ping"+'</li>');}
-     else if (i % 5 === 0) {
-       $('#list').append('<li>'+"pong"+'</li>');}
-     else{
-       $('#list').append('<li>'+i+'</li>');}
-
-   };
+// }
 
  // event.preventDefault();
--- specs/specs.js
+++ specs/specs.js
@@ -2,13 +2,4 @@ describe('pingPong', function() {
    it("identifies numbers divisible by 15", function() {
    expect(pingPong(15)).to.equal("pingpong");
    });
-  it("identifies numbers divisible by 5", function() {
-    expect(pingPong(5)).to.eql("pong")
-  });
-  it("identifies numbers divisible by 3", function() {
-    expect(pingPong(3)).to.eql("ping")
-  });
-  it("counts up to a specified user number", function() {
-    expect(pingPong(2)).to.eql(2)
-  });
 });
