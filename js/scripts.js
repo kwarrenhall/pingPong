390814a8a23659eb753b1c42af846ed21c3aa584
Kate Hall <kwarrenhall@gmail.com>
2016-01-28 22:50:42 -0800

    passing spec and script test on counts up to user number

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,45 @@
+function pingPong(inputNumber) {
+  var numberThree = 3
+  var numberFive = 5
+  var output = [];
 
-var pingPong = function(myNumber) {
+  for (var i = 1; i <= inputNumber; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      output.push("pingpong");
+    } else if (i % 3 === 0) {
+      output.push("ping");
+    } else if (i % 5 === 0) {
+      output.push("pong");
+    } else {
+      output.push(i);
+    }
+  }
+  return output ;
+  };
 
-   for (var i = 1; i <= myNumber; i++) {
-     if (i % 15 === 0) {
-     myNumber = ""
-     myNumber += "pingPong"
-     return myNumber;
-   }
-};
-};
 
- // event.preventDefault();
+
+
+
+
+
+// function pingPong(number) {
+// var output = [];
+//
+//      for (var i = 1; i <= number; i++) {
+//      if (i % 15 === 0) {
+//      output += "PingPong";
+//      } else if (i % 5 === 0) {
+//      output += "pong";
+//      } else if (i % 3 === 0) {
+//      output += "ping";
+//      } else {
+//      output += i;
+//      }
+//      }
+//      return output;
+// };
+//
+//
+//  // event.preventDefault();
+//  for (var i = 1; i <= myNumber; i++) {
--- specs/specs.js
+++ specs/specs.js
@@ -1,8 +1,14 @@
 describe('pingPong', function() {
    it("identifies numbers divisible by 15", function() {
-   expect(pingPong(15)).to.equal("pingPong");
-   });
-   it("identifies numbers divisible by 5", function() {
-   expect(pingPong(5)).to.eql("pong")
-   });
+   expect(pingPong(15)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong"]);
+   })
+ it("identifies numbers divisible by five and returns pong", function(){
+   expect(pingPong(5)).to.eql([1, 2, "ping", 4, 'pong']);
+ })
+ it("identifies numbers divisible by five and returns ping", function(){
+   expect(pingPong(3)).to.eql([1,2,"ping"]);
+ });
+ it("counts up to a specified user number", function () {
+   expect(pingPong(2)).to.eql([1, 2]);
+ });
 });
