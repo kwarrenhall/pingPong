8a2da82951d96502ec52c0090be11ef0f7da88bd
Kate Hall <kwarrenhall@gmail.com>
2016-01-28 20:42:03 -0800

    passing scripts test for divisible by 15

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 
-// var pingPong = function (myNumber) {
-//
-//    for (var i = 1; i <= myNumber; i++) {
-//      if (i % 15 === 0) {
-//      myNumber = ""
-//      myNumber += "pingpong"
-//      return myNumber;
-//    }
-// }
+var pingPong = function(myNumber) {
+
+   for (var i = 1; i <= myNumber; i++) {
+     if (i % 15 === 0) {
+     myNumber = ""
+     myNumber += "pingPong"
+     return myNumber;
+   }
+};
+};
 
  // event.preventDefault();
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,5 @@
 describe('pingPong', function() {
    it("identifies numbers divisible by 15", function() {
-   expect(pingPong(15)).to.equal("pingpong");
+   expect(pingPong(15)).to.equal("pingPong");
    });
 });
