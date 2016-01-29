7349fe688ccd3d13bd3c5d10f7078c3ef9ec3827
Kate Hall <kwarrenhall@gmail.com>
2016-01-28 20:49:17 -0800

    passing spec test on divisible by 5

--- specs/specs.js
+++ specs/specs.js
@@ -2,4 +2,7 @@ describe('pingPong', function() {
    it("identifies numbers divisible by 15", function() {
    expect(pingPong(15)).to.equal("pingPong");
    });
+   it("identifies numbers divisible by 5", function() {
+   expect(pingPong(5)).to.eql("pong")
+   });
 });
